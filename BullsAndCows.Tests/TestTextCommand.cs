94445417bce1bc2535bd2275c6ab21b8e9b6dcb9
Slavi <slavicvetanov@gmail.com>
2014-07-27 20:55:52 +0300

    Text command unit tests

---
+++ BullsAndCows.Tests/TestTextCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BullsAndCows.Exceptions;
+
+namespace BullsAndCows.Tests
+{
+    [TestClass]
+    public class TestTextCommand
+    {
+        [TestMethod]
+        public void TestTextCommandWithValidSetCommand()
+        {
+            TextCommand txtComm = new TextCommand(Engine.GetInstance());
+            txtComm.CommandValue = "help";
+
+            Assert.AreEqual(txtComm.CommandValue, "help", "The command value must be help");
+        }
+
+        [TestMethod]
+        public void TestTextCommandWithValidSetCommandWithBigAndSmallLetters()
+        {
+            TextCommand txtComm = new TextCommand(Engine.GetInstance());
+            txtComm.CommandValue = "hElP";
+
+            Assert.AreEqual(txtComm.CommandValue, "help", "The command value must be help");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidCommandException), "Invalid text command!")]
+        public void TestDigitCommandWithInvalidNumberLength()
+        {
+
+            TextCommand txtComm = new TextCommand(Engine.GetInstance());
+            txtComm.CommandValue = "HelPs";
+        }
+    }
+}
